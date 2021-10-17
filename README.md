a02277e9b39a5105e8083d167c8e2a56123540b3
Deepanshu sunwal <dsunwal@gmail.com>
2021-10-17 23:57:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+
+# CryptoNivesh(The react App)
+
+CryptoNivesh is a static website Which Provide Information about
+various Cryptocurrencies like their current price , Market capitalization,
+Volumes Traded etc. This App is also useful for viewing chart of Different Cryptocurrencies.
+
+
+
+## Screenshots
+
+![App Screenshot](./ss/ss1.png)
+![App Screenshot](./ss/ss2.png)
+![App Screenshot](./ss/ss3.png)
+
+  
+## Built With
+* HTML
+* CSS
+* JAVASCRIPT
+* REACT, REDUX
+* CHART.JS
+* RAPID API
+
+
+  
+## Features
+
+- Provides Charts for Different Crypto Currencies
+- Provides News about crytocurrencies.
+- Gives information about all the cryptocurrencies exchanges
+- Toggle menu in small screen
+- Search News
+- Selected crypto search News
+- Search cryptocurrencies
+- Responsive
+
+
+  
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': 'f0e178dc7emsh45c1308e024e951p143e3bjsn804ef5dae69c',
+};
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
